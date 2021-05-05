92aa510b1d5e5359614c13c98a23243df9d5dddc
Akshay A <adominic21@gmail.com>
2021-05-05 14:10:30 +0530

    removed lombok from the application

--- Backend/src/main/java/com/shopx/ecommerce_app/Entity/Product.java
+++ Backend/src/main/java/com/shopx/ecommerce_app/Entity/Product.java
@@ -17,11 +17,11 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 
-import lombok.Data;
+
 
 @Entity
 @Table(name="product")
-@Data
+
 public class Product{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -52,6 +52,74 @@ public class Product{
     @Column(name="last_updated")
     @UpdateTimestamp
     private Date lastupdated;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public ProductCategory getCategory() {
+        return category;
+    }
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+    }
+    public String getSku() {
+        return sku;
+    }
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public BigDecimal getUnitprice() {
+        return unitprice;
+    }
+    public void setUnitprice(BigDecimal unitprice) {
+        this.unitprice = unitprice;
+    }
+    public String getImageurl() {
+        return imageurl;
+    }
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    public int getUnitsinstock() {
+        return unitsinstock;
+    }
+    public void setUnitsinstock(int unitsinstock) {
+        this.unitsinstock = unitsinstock;
+    }
+    public Date getDatecreated() {
+        return datecreated;
+    }
+    public void setDatecreated(Date datecreated) {
+        this.datecreated = datecreated;
+    }
+    public Date getLastupdated() {
+        return lastupdated;
+    }
+    public void setLastupdated(Date lastupdated) {
+        this.lastupdated = lastupdated;
+    }
+
+    
 
     
 }
