dcaed6212b7c95a09b19ca7ed3baa414c9c1362d
Akshay A <adominic21@gmail.com>
2021-05-05 13:06:30 +0530

    test

--- Backend/src/main/java/com/shopx/ecommerce_app/Repository/ProductCategoryRepo.java
+++ Backend/src/main/java/com/shopx/ecommerce_app/Repository/ProductCategoryRepo.java
@@ -4,7 +4,9 @@ import com.shopx.ecommerce_app.Entity.ProductCategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory",path = "product-category")
 public interface ProductCategoryRepo extends JpaRepository<ProductCategory,Long>{
     
--- Backend/src/main/java/com/shopx/ecommerce_app/Repository/ProductRepo.java
+++ Backend/src/main/java/com/shopx/ecommerce_app/Repository/ProductRepo.java
@@ -3,7 +3,8 @@ package com.shopx.ecommerce_app.Repository;
 import com.shopx.ecommerce_app.Entity.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepo extends JpaRepository<Product,Long>{
     
 }
