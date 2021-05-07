9f04e57e80fe88a7a8e8d9f0b90e491db6c94999
Akshay A <adominic21@gmail.com>
2021-05-07 16:35:47 +0530

    added Query for category

--- Backend/src/main/java/com/shopx/ecommerce_app/Repository/ProductRepo.java
+++ Backend/src/main/java/com/shopx/ecommerce_app/Repository/ProductRepo.java
@@ -2,9 +2,13 @@ package com.shopx.ecommerce_app.Repository;
 
 import com.shopx.ecommerce_app.Entity.Product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepo extends JpaRepository<Product,Long>{
+    Page<Product> findByCategoryId(@RequestParam("id")Long id,Pageable pageable);
     
 }
